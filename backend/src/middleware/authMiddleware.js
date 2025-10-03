aa0215b827ca2f0c0b97c0ddb6e1f421156c676b
Aman Singh <coolaman6299@gmail.com>
2025-10-03 15:35:15 +0530

    Added logic for authMiddleware function

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const { verifyToken } = require("../utils/jwtHelper");
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if(!token) return res.status(401).json({ message: "Unauthorized"});
+
+    try{
+        const decoded = verifyToken(token);
+        req.user = decoded;
+        next();
+    }
+    catch(err) {
+        return res.status(403).json({ message: "Invalid Token "});
+    }
+};
+
+module.exports = authMiddleware;
---
+++ backend/src/utils/jwtHelper.js
@@ -0,0 +1,20 @@
+// Imported all required packages
+const jwt = require("jsonwebtoken");
+
+// Function for generating a token
+const generateToken = (user) => {
+  return jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
+
+// Function to verify a token
+const verifyToken = (token) => {
+    return jwt.verify(token, process.env.JWT_SECRET);
+}
+
+// Exported token generation and verifying functions
+module.exports = {
+  generateToken,
+  verifyToken
+}
